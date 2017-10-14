b57d837fa3801f7cde6f83f6a29ff9bcd686fa6e
ssoonsl <ssoonsl616+gh@gmail.com>
2017-10-14 17:38:29 +0800

    Add Team seeds

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,8 @@ class Team < ApplicationRecord
   belongs_to :student
   belongs_to :teammate, class_name: 'Student', foreign_key: :teammate_id
 
+  def self.permutate_team_into_pairs(groups)
+    groups.map { |group| group.permutation(2).to_a }.flatten(1)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,10 @@ puts 'Seeding students..'
 student_names.each do |student_name|
   Student.create(name: student_name)
 end
+
+shopping_cart_teams = [['Haris', 'Nicole', 'Sarah'], ['Jacinta', 'Kelvin', 'Shafiq'], ['Sherlynn', 'Regent'], ['Vincent', 'Nazmi', 'KheeShian']]
+
+puts 'Seeding teams..'
+Team.permutate_team_into_pairs(shopping_cart_teams).each do |pair|
+  Team.create(student: Student.find_by(name: pair[0]), teammate: Student.find_by(name: pair[1]))
+end
